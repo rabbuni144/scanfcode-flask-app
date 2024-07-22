221cd4237f9406014dcbb434a648f278733a9609
Rabbuni Merugumala <rabbuni144@gmail.com>
2024-07-22 15:16:47 +0530

    App.py updated

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask import Flask,render_template
 #!  WSGI application
 app=Flask(__name__)
 
+# main app route
 @app.route('/')
 def welcome():
     return render_template("index.html")
 
+# a rote to add new students
 @app.route("/newstudent")
 def addstudent():
     return "/"
